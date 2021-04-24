3e6705235118951ed32adb5d0f0362576c3ec081
Douglas Lopes <douglasdmlopes@gmail.com>
2021-04-24 13:23:41 -0300

    Header plant manager

---
+++ src/components/Header.tsx
@@ -0,0 +1,59 @@
+import { getActionFromState } from '@react-navigation/core';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  Image,
+  View
+} from 'react-native';
+
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import userImg from '../assets/profile.jpg';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Douglas</Text>
+      </View>
+
+      <Image 
+        style={styles.image}
+        source={userImg}
+      />
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: getStatusBarHeight(),
+    paddingVertical: 20,
+    //backgroundColor: colors.red
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  }
+});
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -4,12 +4,24 @@ import {
   Text,
   StyleSheet
 } from 'react-native';
+
+import { Header } from '../components/Header';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSelect() {
   return (
     <View style={styles.container}>
-      <Text>Selecionar Planta</Text>
+      <View style={styles.header}>
+        <Header/>
+        <Text style={styles.title}>
+          Em qual ambiente
+        </Text>
+        <Text style={styles.subtitle}>
+          Você quer colocar sua planta?
+        </Text>
+      </View>
+      
     </View>
   )
 }
@@ -18,9 +30,26 @@ const styles = StyleSheet.create({
   
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    //justifyContent: 'center',
+    //alignItems: 'center',
     backgroundColor: colors.background
   }, 
+  header: {
+    paddingHorizontal: 30
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading
+  },
+  
 
 });
