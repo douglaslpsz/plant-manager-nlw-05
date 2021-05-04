501cd16952fc29e3652b0b48112de8330bca7af9
Douglas Lopes <douglasdmlopes@gmail.com>
2021-05-04 09:12:38 -0300

    Fix

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -127,6 +127,7 @@ export function PlantSelect() {
       <View>
         <FlatList
           data={enviroments}
+          keyExtractor={(item) => String(item.key)}
           renderItem={({item}) => (
             <EnviromentButton 
               title={item.title}
@@ -143,6 +144,7 @@ export function PlantSelect() {
       <View style={styles.plants}>
         <FlatList 
           data={filteredPlants}
+          keyExtractor={(item) => String(item.id)}
           renderItem={ ( {item} ) => (
             <PlantCardPrimary data={item} />
           )}
