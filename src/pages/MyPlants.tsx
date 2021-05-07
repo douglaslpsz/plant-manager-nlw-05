399f141f24ec3446ea72030b74e40e694c6668c5
Douglas Lopes <douglasdmlopes@gmail.com>
2021-05-07 12:27:08 -0300

    Fix style

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -118,9 +118,7 @@ export function MyPlants() {
           (
             <Text style={styles.noContent}>Nenhum agendamento criado!</Text>
           )
-        }
-      
-      
+        }      
     </View>
   )
 }
@@ -131,7 +129,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     paddingHorizontal: 30,
-    paddingTop: 50,
+    //paddingTop: 50,
     backgroundColor: colors.background
   },
   spotlight: {
