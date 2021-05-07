cac5a73e76b1827e4e3487a1b1725e53b2c7f561
Douglas Lopes <douglasdmlopes@gmail.com>
2021-05-07 12:20:46 -0300

    Fix

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppLoading from 'expo-app-loading';
+import * as Notifications from 'expo-notifications';
 
 import { Welcome } from './src/pages/Welcome';
 import { UserIdentification } from './src/pages/UserIdentification';
@@ -10,12 +11,32 @@ import {
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
+import { PlantProps } from './src/libs/storage';
 
 export default function App() {
 
   const [fonstLoaded ] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
+  });
+
+  useEffect( () => {
+
+    const subscription = Notifications.addNotificationReceivedListener(
+      async notification => {
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data);
+      });
+
+      return () => subscription.remove();
+
+      /*async function notifications() {
+        const data = await Notifications.getAllScheduledNotificationsAsync();
+
+        console.log('######## NOTIFICAÇÕES AGENDADAS ##########');
+        console.log(data);
+      }*/
+
   });
 
   if(!fonstLoaded) { //Se a fonte não foi carregada
